90993ef36925fcf660cad56cae5f31261c1ee646
Antal Nagy <nagy.antal@uni-eszterhazy.hu>
2025-04-05 07:39:35 +0200

    Adding missing imports after NatSpec comments and changing OpenZeppelin package version

--- contracts/TonysSepoliaFaucet.sol
+++ contracts/TonysSepoliaFaucet.sol
@@ -1,5 +1,9 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
 
 /**
  * @title Tony's Sepolia Faucet
